f4b6be06998841f1c286d3969b47b03a24a4f575
Michele922 <Michelesijbring@hotmail.com>
2022-11-08 13:45:59 +0100

    Homepage with navbar and filter

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar/Navbar";
+import Filter from "../../components/Navbar/Filter/Filter";
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Filter />
+    </div>
+  );
+};
+export default Home;
