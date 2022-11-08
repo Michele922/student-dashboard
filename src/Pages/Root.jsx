48da3eb743d8799abc94c77443f7370a98f0fb08
Michele922 <Michelesijbring@hotmail.com>
2022-11-08 15:41:34 +0100

    Added route and error page

---
+++ src/Pages/Root.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Students from "./Students/Students";
+import Home from "./Home/Home";
+
+export default function Root() {
+  return (
+    <>
+      <Home />
+      <Students />
+    </>
+  );
+}
