49cb91be2e66263516b7ba65b5afcaa8d0538304
Michele922 <Michelesijbring@hotmail.com>
2022-11-08 15:47:51 +0100

    Added another page

---
+++ src/Pages/Students/Students.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar/Navbar";
+import "./Students.css";
+import SelectFilter from "../../components/SelectFilter/SelectFilter";
+
+const Students = () => {
+  return (
+    <div>
+      <Navbar />
+      <SelectFilter />
+    </div>
+  );
+};
+export default Students;
